5550b3c339a9828866efd71c61abdd348a46214e
Jerishajerald <92445299+Jerishajerald@users.noreply.github.com>
2022-04-29 17:57:42 +0530

    Add files via upload

---
+++ App2.java
@@ -0,0 +1,19 @@
+package com.training;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class App2 {
+
+	public static void main(String[] args) {
+		
+		List<String> names = Arrays.asList("India","Srilanka","Indonesia","Japan","Bhutan");
+	   // names.forEach(value -> System.out.println(value));
+		
+		names.forEach(System.out::println);
+	    
+	    
+	
+	}
+
+}
